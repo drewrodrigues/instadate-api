e422c1c78e732a68bf27eead8c184aa33e6e0793
Drew <thesimpledev@gmail.com>
2019-11-18 17:01:30 -0800

    Add seed images and date for instadates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,30 @@
 ActiveRecord::Base.transaction do
+  Picture.destroy_all
   Instadate.destroy_all
   User.destroy_all
 
-  User.create!(
-    email: 'drew@example.com',
-    password_digest: '$2a$12$nBoKjl6D3dgjDAtoZ9q7U.Rp64Xv.ZWSpexP5T/rdiNHb1QOSlBKW',
-    name: 'Drew',
-    age: 24,
-    location: 'Walnut Creek, CA',
-    sex: 'man',
-    interested_in: ['women'],
-    outcomes: ['dating', 'relationship'],
-    bio: 'Something cool goes here'
-  )
+  20.times do |i|
+    photo = Picture.new
+    photo.file.attach(io: File.open(File.join(Rails.root, "db/seed_data/#{i+1}.jpeg")), filename: i)
+    photo.save
 
-  Instadate.create!(
-    creator: User.last,
-    location: 'San Francisco, CA',
-    activity: 'movie'
-  )
+    User.create!(
+      email: "drew#{i}@example.com",
+      password_digest: '$2a$12$nBoKjl6D3dgjDAtoZ9q7U.Rp64Xv.ZWSpexP5T/rdiNHb1QOSlBKW',
+      name: "Drew #{i}",
+      age: 24,
+      location: 'Walnut Creek, CA',
+      sex: 'man',
+      interested_in: ['women'],
+      outcomes: ['dating', 'relationship'],
+      bio: 'Something cool goes here',
+      picture: photo
+    )
+
+    Instadate.create!(
+      creator: User.last,
+      location: 'San Francisco, CA',
+      activity: Instadate::ACTIVITIES.sample
+    )
+  end
 end
