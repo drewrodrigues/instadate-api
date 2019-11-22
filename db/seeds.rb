1fa21b5ae4619c70f53bc3b9b54f4815258444d5
Drew <thesimpledev@gmail.com>
2019-11-21 17:05:40 -0800

    Add seed data for male and females w/ images

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,20 @@ ActiveRecord::Base.transaction do
   Instadate.destroy_all
   User.destroy_all
 
-  20.times do |i|
+  (1..40).each do |i|
     photo = Picture.new
-    photo.file.attach(io: File.open(File.join(Rails.root, "db/seed_data/#{i+1}.jpeg")), filename: i)
+    photo.file.attach(io: File.open(File.join(Rails.root, "db/seed_data/#{i}.jpeg")), filename: i)
     photo.save
 
     User.create!(
       email: "drew#{i}@example.com",
-      password_digest: '$2a$12$nBoKjl6D3dgjDAtoZ9q7U.Rp64Xv.ZWSpexP5T/rdiNHb1QOSlBKW',
-      name: "Drew #{i}",
+      password_digest: '$2a$12$nBoKjl6D3dgjDAtoZ9q7U.Rp64Xv.ZWSpexP5',
+      name: i > 20 ? "Girl Name #{i}" : "Guy Name #{i}",
       age: 24,
       location: 'Walnut Creek, CA',
-      sex: 'man',
-      interested_in: ['women'],
-      outcomes: ['dating', 'relationship'],
+      sex: i > 20 ? 'woman' : 'man',
+      interested_in: i > 20 ? ['men'] : ['women'],
+      outcomes: %w[dating relationship],
       bio: 'Something cool goes here',
       picture: photo
     )
