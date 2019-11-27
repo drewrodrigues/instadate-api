ffeeaa1eba5ff55b44b5e697354a050e70c170f2
Drew <thesimpledev@gmail.com>
2019-11-26 20:34:05 -0800

    Take session token from authorization header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::API
   def current_user
-    @current_user ||= User.find_by(session_token: session[:session_token])
+    @current_user ||= User.find_by(session_token: session_token)
+  end
+
+  private
+
+  def session_token
+    request.authorization
   end
 end
