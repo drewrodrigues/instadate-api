d6251b00654ccc46f18aa85a0474ecc2344f1f11
Drew <thesimpledev@gmail.com>
2020-02-10 20:00:36 -0800

    Annotate

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id              :integer          not null, primary key
+#  body            :text             not null
+#  conversation_id :integer          not null
+#  user_id         :integer          not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_messages_on_conversation_id  (conversation_id)
+#  index_messages_on_user_id          (user_id)
+#
+
 class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :user
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id              :integer          not null, primary key
+#  body            :text             not null
+#  conversation_id :integer          not null
+#  user_id         :integer          not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_messages_on_conversation_id  (conversation_id)
+#  index_messages_on_user_id          (user_id)
+#
+
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
